bff968d4fd7134f997cb2d5f0a4b0a82600066a1
hackifiery <129475587+hackifiery@users.noreply.github.com>
2025-10-06 19:39:23 +0000

    move function defs from headers

--- src/keyboard.hpp
+++ src/keyboard.hpp
@@ -1,28 +1,3 @@
-#include <iostream>
-#include <unistd.h>
-
 #pragma once
 
-using namespace std;
-
-char readKey() {
-    char c;
-    if (read(STDIN_FILENO, &c, 1) != 1) return 0;
-
-    if (c == 27) { // Escape sequence
-        char seq[2];
-        if (read(STDIN_FILENO, &seq[0], 1) != 1) return 27;
-        if (read(STDIN_FILENO, &seq[1], 1) != 1) return 27;
-
-        if (seq[0] == '[') {
-            switch (seq[1]) {
-                case 'A': return 'U'; // Up
-                case 'B': return 'D'; // Down
-                case 'C': return 'R'; // Right
-                case 'D': return 'L'; // Left
-                default: return '?';
-            }
-        }
-    }
-    return c;
-}
+char readKey();
