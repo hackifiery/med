1fc549fa267ee9cc9732fea0125785f1759b8294
hackifiery <xiaofeifei2011@gmail.com>
2025-10-16 21:51:05 -0700

    added home/end support (ctrl keys NOT WORKING)

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <unistd.h>
 
-#include "keyboard.hpp"
-
 using namespace std;
 
 char readKey() {
@@ -10,19 +8,29 @@ char readKey() {
     if (read(STDIN_FILENO, &c, 1) != 1) return 0;
 
     if (c == 27) { // Escape sequence
-        char seq[2];
-        if (read(STDIN_FILENO, &seq[0], 1) != 1) return 27;
-        if (read(STDIN_FILENO, &seq[1], 1) != 1) return 27;
+        char seq[6];
+        // read up to 5 bytes after ESC
+        int n = read(STDIN_FILENO, &seq, sizeof(seq));
 
-        if (seq[0] == '[') {
-            switch (seq[1]) {
-                case 'A': return 'U'; // Up
-                case 'B': return 'D'; // Down
-                case 'C': return 'R'; // Right
-                case 'D': return 'L'; // Left
-                default: return '?';
+        if (n >= 2 && seq[0] == '[') {
+            if (n == 2) {
+                switch (seq[1]) {
+                    case 'A': return 'U'; // Up
+                    case 'B': return 'D'; // Down
+                    case 'C': return 'R'; // Right
+                    case 'D': return 'L'; // Left
+                    case 'H': return 'H'; // Home
+                    case 'F': return 'E'; // End
+                }
+            }
+            else if (n >= 5 && seq[0] == '1' && seq[1] == ';' && seq[2] == '5') {
+                if (seq[3] == 'H') return 'h'; // Ctrl+Home
+                if (seq[3] == 'F') return 'e'; // Ctrl+End
             }
         }
+
+        return '?'; // unknown escape
     }
+
     return c;
 }
